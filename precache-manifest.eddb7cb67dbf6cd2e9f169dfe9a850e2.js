753569197fcccf8e32a35420774e532d02f7753b
Anmols007 <anmolsharma913@yahoo.com>
2020-07-26 15:05:44 +0530

    Updates

---
+++ precache-manifest.eddb7cb67dbf6cd2e9f169dfe9a850e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ffea6bb8dccd42aa2b86c9157d5820a",
+    "url": "/Calendar/static/media/logo.1ffea6bb.png"
+  },
+  {
+    "revision": "b1af0aebea77df3a062c",
+    "url": "/Calendar/static/js/runtime~main.81f9921a.js"
+  },
+  {
+    "revision": "2cff83ed4e10ba0bbe62",
+    "url": "/Calendar/static/js/main.9e91264c.chunk.js"
+  },
+  {
+    "revision": "0f4b0e06558bc8e359f6",
+    "url": "/Calendar/static/js/2.c926e784.chunk.js"
+  },
+  {
+    "revision": "2cff83ed4e10ba0bbe62",
+    "url": "/Calendar/static/css/main.90c71158.chunk.css"
+  },
+  {
+    "revision": "0db7aeaaf28651f17fb34f33a0f3beda",
+    "url": "/Calendar/index.html"
+  }
+];
